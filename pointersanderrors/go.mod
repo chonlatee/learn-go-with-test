90e0a7e1e7217904b216dafa47898f3a59544289
Chonlatee Jumratsee <chonlatee.j@lmwn.com>
2021-05-04 00:07:51 +0700

    Chapter: Pointers and errors - create assertNoError and move helper function outside test

--- pointersanderrors/go.mod
+++ pointersanderrors/go.mod
@@ -1,3 +1,10 @@
 module lgwt/pterr
 
 go 1.16
+
+require (
+	github.com/kisielk/errcheck v1.6.0 // indirect
+	golang.org/x/mod v0.4.2 // indirect
+	golang.org/x/sys v0.0.0-20210503080704-8803ae5d1324 // indirect
+	golang.org/x/tools v0.1.0 // indirect
+)
--- pointersanderrors/wallet_test.go
+++ pointersanderrors/wallet_test.go
@@ -6,25 +6,6 @@ import (
 
 func TestWallet(t *testing.T) {
 
-	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
-		t.Helper()
-		got := wallet.Balance()
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
-	}
-
-	assertError := func(t testing.TB, got error, want string) {
-		t.Helper()
-		if got == nil {
-			t.Error("wanted an error but didn't get one")
-		}
-
-		if got.Error() != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
-	}
-
 	t.Run("Deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10))
@@ -34,8 +15,9 @@ func TestWallet(t *testing.T) {
 
 	t.Run("Withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
-		wallet.Withdraw(Bitcoin(10))
+		err := wallet.Withdraw(Bitcoin(10))
 		want := Bitcoin(10)
+		assertNoError(t, err)
 		assertBalance(t, wallet, want)
 	})
 
@@ -45,7 +27,33 @@ func TestWallet(t *testing.T) {
 		err := wallet.Withdraw(Bitcoin(200))
 
 		assertBalance(t, wallet, startingBalance)
-		assertError(t, err, "cannot withdraw, insufficient funds")
+		assertError(t, err, ErrInsufficientFunds)
 	})
 
+}
+
+func assertBalance(t testing.TB, wallet Wallet, want Bitcoin) {
+	t.Helper()
+	got := wallet.Balance()
+	if got != want {
+		t.Errorf("got %s want %s", got, want)
+	}
+}
+
+func assertError(t testing.TB, got error, want error) {
+	t.Helper()
+	if got == nil {
+		t.Error("wanted an error but didn't get one")
+	}
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
+func assertNoError(t testing.TB, got error) {
+	t.Helper()
+	if got != nil {
+		t.Fatal("get an error but did't want one")
+	}
 }
