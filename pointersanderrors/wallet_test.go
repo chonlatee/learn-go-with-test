526a2ec5d7a9c30edf0a1ceed014fdd14803299c
Chonlatee <jumratsee@gmail.com>
2021-05-03 17:25:18 +0700

    Chapter: Pointers and errors - add test withdraw

--- pointersanderrors/wallet_test.go
+++ pointersanderrors/wallet_test.go
@@ -6,14 +6,30 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-	wallet.Deposit(Bitcoin(10))
-	got := wallet.Balance()
-	want := Bitcoin(10)
 
-	fmt.Printf("address of balance in test is %v \n", &wallet.balance)
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		got := wallet.Balance()
+		want := Bitcoin(10)
 
-	if got != want {
-		t.Errorf("got %s want %s", got, want)
-	}
+		fmt.Printf("address of balance in test is %v \n", &wallet.balance)
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		wallet.Withdraw(Bitcoin(10))
+
+		got := wallet.Balance()
+		want := Bitcoin(10)
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
+
 }
