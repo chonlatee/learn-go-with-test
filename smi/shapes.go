4000ad7f1bb77711406947d1fb7544376c34514a
Chonlatee <jumratsee@gmail.com>
2021-05-03 12:40:56 +0700

    Chapter: Structs, methods, & interface - add triangle in areaTests struct

--- smi/shapes.go
+++ smi/shapes.go
@@ -24,6 +24,15 @@ func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
 }
 
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
+}
+
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
 }
--- smi/shapes_test.go
+++ smi/shapes_test.go
@@ -22,6 +22,7 @@ func TestArea(t *testing.T) {
 	}{
 		{shape: Rectangle{12.0, 6.0}, want: 72.0},
 		{shape: Circle{10}, want: 314.1592653589793},
+		{shape: Triangle{12, 6}, want: 36.0},
 	}
 
 	for _, tt := range areaTests {
