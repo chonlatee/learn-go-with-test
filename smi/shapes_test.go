80833da090d6b31943e6f0e7fd636aedf7679e12
Chonlatee <jumratsee@gmail.com>
2021-05-03 09:41:04 +0700

    Chapter: Structs, methods, & interface - introduce table test driven

--- smi/shapes_test.go
+++ smi/shapes_test.go
@@ -16,22 +16,18 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 
-	checkArea := func(t testing.TB, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{shape: Rectangle{12.0, 6.0}, want: 72.0},
+		{shape: Circle{10}, want: 314.1592653589793},
 	}
 
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12.0, 6.0}
-		checkArea(t, rectangle, 72.0)
-	})
-
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		checkArea(t, circle, 314.1592653589793)
-
-	})
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %g but want %g", got, tt.want)
+		}
+	}
 }
