224d0e64d7b7343fe11031bbffe94ad4189a2215
Chonlatee <jumratsee@gmail.com>
2021-05-03 17:30:28 +0700

    Chapter: Pointers and errors - refactor add assertBalance function helper

--- pointersanderrors/wallet_test.go
+++ pointersanderrors/wallet_test.go
@@ -1,35 +1,31 @@
 package wallet
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestWallet(t *testing.T) {
 
-	t.Run("Deposit", func(t *testing.T) {
-		wallet := Wallet{}
-		wallet.Deposit(Bitcoin(10))
+	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
+		t.Helper()
 		got := wallet.Balance()
-		want := Bitcoin(10)
-
-		fmt.Printf("address of balance in test is %v \n", &wallet.balance)
-
 		if got != want {
 			t.Errorf("got %s want %s", got, want)
 		}
+	}
+
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		want := Bitcoin(10)
+		assertBalance(t, wallet, want)
 	})
 
 	t.Run("Withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
 		wallet.Withdraw(Bitcoin(10))
-
-		got := wallet.Balance()
 		want := Bitcoin(10)
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertBalance(t, wallet, want)
 	})
 
 }
