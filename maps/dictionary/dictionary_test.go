b46413bdce2a87a2a971d27d888fef551787e8ee
Chonlatee Jumratsee <chonlatee.j@lmwn.com>
2021-05-04 13:38:27 +0700

    Chapter: Maps - add search error if search not found

--- maps/dictionary/dictionary_test.go
+++ maps/dictionary/dictionary_test.go
@@ -4,10 +4,23 @@ import "testing"
 
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
-	got := dictionary.Search("test")
-	want := "this is just a test"
 
-	assertString(t, got, want)
+	t.Run("know word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
+		assertString(t, got, want)
+	})
+
+	t.Run("unknow word", func(t *testing.T) {
+		_, err := dictionary.Search("unknow")
+		want := "could not find the word you were looking for"
+
+		if err == nil {
+			t.Fatal("expected to get an error.")
+		}
+
+		assertString(t, err.Error(), want)
+	})
 
 }
 
