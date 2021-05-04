f44318eefb92b66401ec99b0735319f8f3a3cb5e
Chonlatee Jumratsee <chonlatee.j@lmwn.com>
2021-05-04 14:20:51 +0700

    Chapter: Maps - create add function in dictionary

--- maps/dictionary/dictionary_test.go
+++ maps/dictionary/dictionary_test.go
@@ -21,6 +21,22 @@ func TestSearch(t *testing.T) {
 
 }
 
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+	dictionary.Add("test", "this is just a test")
+
+	want := "this is just a test"
+	got, err := dictionary.Search("test")
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+
+}
+
 func assertString(t testing.TB, got, want string) {
 	t.Helper()
 
