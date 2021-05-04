4a43f69a14d1231067723753a46a914252d496b4
Chonlatee Jumratsee <chonlatee.j@lmwn.com>
2021-05-04 23:48:47 +0700

    Chapter: Mocking - sleep 1 second for each count

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -4,13 +4,18 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
+const finalWord = "Go!"
+const countdownStart = 3
+
 func Countdown(out io.Writer) {
-	for i := 3; i > 0; i-- {
+	for i := countdownStart; i > 0; i-- {
+		time.Sleep(1 * time.Second)
 		fmt.Fprintln(out, i)
 	}
-	fmt.Fprint(out, "Go!")
+	fmt.Fprint(out, finalWord)
 }
 
 func main() {
