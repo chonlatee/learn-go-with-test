e40f21f5b6cfa81f974f161013dfa8e6fb947dea
Chonlatee Jumratsee <chonlatee.j@lmwn.com>
2021-05-05 00:22:24 +0700

    Chapter: Mocking - add test print 321GO

--- mocking/countdown_test.go
+++ mocking/countdown_test.go
@@ -1,11 +1,28 @@
 package main
 
 import (
+	"bytes"
 	"reflect"
 	"testing"
 )
 
 func TestCountdown(t *testing.T) {
+	t.Run("prints 3 to Go!", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+		Countdown(buffer, &CountdownOperationsSpy{})
+
+		got := buffer.String()
+		want := `3
+2
+1
+Go!`
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+
+	})
+
 	t.Run("sleep before every print", func(t *testing.T) {
 		spySleepPrinter := &CountdownOperationsSpy{}
 		Countdown(spySleepPrinter, spySleepPrinter)
