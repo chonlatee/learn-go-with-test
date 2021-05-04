4ff92959e6dc3ad59ff5f1b9093a6f6c76dbba61
Chonlatee Jumratsee <chonlatee.j@lmwn.com>
2021-05-04 15:27:00 +0700

    Chapter: Maps - create new test about add existing word

--- maps/dictionary/dictionary.go
+++ maps/dictionary/dictionary.go
@@ -1,10 +1,14 @@
 package dictionary
 
-import "errors"
-
 type Dictionary map[string]string
+type DictionaryErr string
 
-var ErrNotFound = errors.New("could not find the word you were looking for")
+var ErrNotFound = DictionaryErr("could not find the word you were looking for")
+var ErrWordExists = DictionaryErr("cannot add word because it already exists")
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
 
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
@@ -14,6 +18,17 @@ func (d Dictionary) Search(word string) (string, error) {
 	return definition, nil
 }
 
-func (d Dictionary) Add(word, definition string) {
-	d[word] = definition
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
 }
--- maps/dictionary/dictionary_test.go
+++ maps/dictionary/dictionary_test.go
@@ -22,11 +22,25 @@ func TestSearch(t *testing.T) {
 }
 
 func TestAdd(t *testing.T) {
-	dictionary := Dictionary{}
-	word := "test"
-	definition := "this is just a test"
-	dictionary.Add(word, definition)
-	assertDefinition(t, dictionary, word, definition)
+
+	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		word := "test"
+		definition := "this is just a test"
+		dictionary.Add(word, definition)
+		assertDefinition(t, dictionary, word, definition)
+	})
+
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+		err := dictionary.Add(word, "new test")
+
+		assertError(t, err, ErrWordExists)
+		assertDefinition(t, dictionary, word, definition)
+	})
+
 }
 
 func assertString(t testing.TB, got, want string) {
