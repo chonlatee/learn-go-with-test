85d5abfc9ec45d837aa3ebe9069500acf91c51a7
Chonlatee Jumratsee <chonlatee.j@lmwn.com>
2021-05-06 00:24:05 +0700

    Chapter: Concurrency - make it pass and make it fast

--- concurrency/concurrency.go
+++ concurrency/concurrency.go
@@ -1,19 +1,26 @@
 package concurrency
 
-import "time"
-
 type WebsiteChecker func(string) bool
+
+type result struct {
+	string
+	bool
+}
 
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	resultChannel := make(chan result)
 
 	for _, url := range urls {
 		go func(u string) {
-			results[u] = wc(u)
+			resultChannel <- result{u, wc(u)}
 		}(url)
 	}
 
-	time.Sleep(5 * time.Second)
+	for i := 0; i < len(urls); i++ {
+		r := <-resultChannel
+		results[r.string] = r.bool
+	}
 
 	return results
 }
