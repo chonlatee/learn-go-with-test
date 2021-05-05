4e27219c04acfc0a04987ee82bbcdc1fb538c468
Chonlatee Jumratsee <chonlatee.j@lmwn.com>
2021-05-06 00:43:20 +0700

    Chapter: Select - make Racer with simple solution

--- select/racer.go
+++ select/racer.go
@@ -1,5 +1,22 @@
 package racer
 
+import (
+	"net/http"
+	"time"
+)
+
 func Racer(a, b string) (winner string) {
-	return
+	startA := time.Now()
+	http.Get(a)
+	aDuration := time.Since(startA)
+
+	startB := time.Now()
+	http.Get(b)
+	bDuration := time.Since(startB)
+
+	if aDuration < bDuration {
+		return a
+	}
+
+	return b
 }
