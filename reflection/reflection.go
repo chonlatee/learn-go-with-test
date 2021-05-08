4e7843d2b91ca9cd80d7199a9bffe93d5832032b
Chonlatee Jumratsee <chonlatee.j@lmwn.com>
2021-05-08 18:41:02 +0700

    Chapter: Reflection - Write enough code to make it pass

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -1,5 +1,9 @@
 package reflection
 
+import "reflect"
+
 func walk(x interface{}, fn func(input string)) {
-	fn("i still can't believe South korea beat Germany 2-0 to put them last in their group")
+	val := reflect.ValueOf(x)
+	field := val.Field(0)
+	fn(field.String())
 }
