5489fed9933275fec6ef3d1d366a377a4d43f7a4
Chonlatee Jumratsee <chonlatee.j@lmwn.com>
2021-05-08 23:50:18 +0700

    Chapter: Sync - Write the test first add test concurrently

--- sync/sync_test.go
+++ sync/sync_test.go
@@ -1,6 +1,9 @@
 package sync
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
 
 func TestCounter(t *testing.T) {
 	t.Run("incrementing the counter 3 times leaves it at 3", func(t *testing.T) {
@@ -10,6 +13,26 @@ func TestCounter(t *testing.T) {
 		counter.Inc()
 
 		assertCounter(t, counter, 3)
+
+	})
+
+	t.Run("it runs safely concurrently", func(t *testing.T) {
+		wantedCount := 1000
+		counter := Counter{}
+
+		var wg sync.WaitGroup
+		wg.Add(wantedCount)
+
+		for i := 0; i < wantedCount; i++ {
+			go func(w *sync.WaitGroup) {
+				counter.Inc()
+				w.Done()
+			}(&wg)
+		}
+
+		wg.Wait()
+
+		assertCounter(t, counter, wantedCount)
 
 	})
 }
