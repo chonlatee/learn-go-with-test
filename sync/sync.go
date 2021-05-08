d23ddf64e2d112f567ee2995c6dc4732d85a722d
Chonlatee Jumratsee <chonlatee.j@lmwn.com>
2021-05-08 23:53:13 +0700

    Chapter: Sync - Write enough code to make it pass add mutext to lock value

--- sync/sync.go
+++ sync/sync.go
@@ -1,11 +1,16 @@
 package sync
 
+import "sync"
+
 type Counter struct {
+	mu    sync.Mutex
 	value int
 }
 
 func (c *Counter) Inc() {
+	c.mu.Lock()
 	c.value++
+	c.mu.Unlock()
 }
 
 func (c *Counter) Value() int {
