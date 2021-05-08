04b3f4879918e2bbb2209b36625df9a78794fe7b
Chonlatee Jumratsee <chonlatee.j@lmwn.com>
2021-05-08 19:35:10 +0700

    Chapter: Reflection - Write the test first add nest pointer struct

--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -42,6 +42,14 @@ func TestWalk(t *testing.T) {
 			},
 			[]string{"Chris", "London"},
 		},
+		{
+			"Pointers to thing",
+			&Person{
+				"Chris",
+				Profile{33, "London"},
+			},
+			[]string{"Chris", "London"},
+		},
 	}
 
 	for _, test := range cases {
