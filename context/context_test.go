b779a14895a361e52ec94ef4dfc7f141b4736a70
Chonlatee Jumratsee <chonlatee.j@lmwn.com>
2021-05-09 01:45:49 +0700

    Chapter: Context - Refactor create function helper assert cancel

--- context/context_test.go
+++ context/context_test.go
@@ -9,10 +9,10 @@ import (
 )
 
 func TestServer(t *testing.T) {
+	data := "Hello, world"
 
 	t.Run("return data from store", func(t *testing.T) {
-		data := "Hello, world"
-		store := &SpyStore{response: data}
+		store := &SpyStore{response: data, t: t}
 		svr := Server(store)
 
 		request := httptest.NewRequest(http.MethodGet, "/", nil)
@@ -23,15 +23,11 @@ func TestServer(t *testing.T) {
 		if response.Body.String() != data {
 			t.Errorf(`got "%s", want "%s"`, response.Body.String(), data)
 		}
-
-		if store.cancelled {
-			t.Error("it should not have cancelled the store")
-		}
+		store.assertWasNotCancelled()
 	})
 
 	t.Run("tells store to cancel work if request is cancelled", func(t *testing.T) {
-		data := "hello, world"
-		store := &SpyStore{response: data}
+		store := &SpyStore{response: data, t: t}
 		srv := Server(store)
 
 		request := httptest.NewRequest(http.MethodGet, "/", nil)
@@ -40,20 +36,18 @@ func TestServer(t *testing.T) {
 		time.AfterFunc(5*time.Millisecond, cancel)
 
 		request = request.WithContext(cancellingCtx)
-
 		response := httptest.NewRecorder()
 
 		srv.ServeHTTP(response, request)
 
-		if !store.cancelled {
-			t.Error("store was not told to cancel")
-		}
+		store.assertWasCancelled()
 	})
 }
 
 type SpyStore struct {
 	response  string
 	cancelled bool
+	t         *testing.T
 }
 
 func (s *SpyStore) Fetch() string {
@@ -63,4 +57,18 @@ func (s *SpyStore) Fetch() string {
 
 func (s *SpyStore) Cancel() {
 	s.cancelled = true
+}
+
+func (s *SpyStore) assertWasCancelled() {
+	s.t.Helper()
+	if !s.cancelled {
+		s.t.Error("store was not told to cancel")
+	}
+}
+
+func (s *SpyStore) assertWasNotCancelled() {
+	s.t.Helper()
+	if s.cancelled {
+		s.t.Error("store was told to cancel")
+	}
 }
