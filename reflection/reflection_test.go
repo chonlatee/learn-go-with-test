ffb7be27b3b80e797c9d23e095766c87fdf3861f
Chonlatee Jumratsee <chonlatee.j@lmwn.com>
2021-05-08 22:48:50 +0700

    Chapter: Reflection - Write the test first add test function type

--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -122,6 +122,23 @@ func TestWalk(t *testing.T) {
 
 	})
 
+	t.Run("with function", func(t *testing.T) {
+		aFunction := func() (Profile, Profile) {
+			return Profile{33, "Berlin"}, Profile{34, "Katowice"}
+		}
+
+		var got []string
+		want := []string{"Berlin", "Katowice"}
+
+		walk(aFunction, func(input string) {
+			got = append(got, input)
+		})
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v, want %v", got, want)
+		}
+
+	})
 }
 
 func assertContain(t testing.TB, haystack []string, needle string) {
