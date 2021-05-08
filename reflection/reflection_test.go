4c155343d4e0f096750c679d3393d4e82142d24b
Chonlatee Jumratsee <chonlatee.j@lmwn.com>
2021-05-08 22:24:50 +0700

    Chapter: Reflection - Write the test first add test channel

--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -100,6 +100,27 @@ func TestWalk(t *testing.T) {
 		assertContain(t, got, "Boz")
 	})
 
+	t.Run("with channels", func(t *testing.T) {
+		aChannel := make(chan Profile)
+
+		go func() {
+			aChannel <- Profile{33, "Berline"}
+			aChannel <- Profile{34, "Katowice"}
+		}()
+
+		var got []string
+
+		want := []string{"Berlin", "katowice"}
+		walk(aChannel, func(input string) {
+			got = append(got, input)
+		})
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v, want %v", got, want)
+		}
+
+	})
+
 }
 
 func assertContain(t testing.TB, haystack []string, needle string) {
