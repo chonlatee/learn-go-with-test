7212e5e370e3e9f16a1ee7d3f214ca6cf6616baf
Chonlatee Jumratsee <chonlatee.j@lmwn.com>
2021-05-09 21:08:27 +0700

    Chapter: Intro to property based tests - Refactor use table test driven for sub test

--- basedtests/numeral_test.go
+++ basedtests/numeral_test.go
@@ -3,23 +3,21 @@ package numeral
 import "testing"
 
 func TestRomanNumerals(t *testing.T) {
+	cases := []struct {
+		Description string
+		Arabic      int
+		Want        string
+	}{
+		{"1 gets converted to I", 1, "I"},
+		{"2 gets converted to II", 2, "II"},
+	}
 
-	t.Run("1 gets converted to I", func(t *testing.T) {
-		got := ConvertToRoman(1)
-		want := "I"
-
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
-	})
-
-	t.Run("2 gets converted to II", func(t *testing.T) {
-		got := ConvertToRoman(2)
-		want := "II"
-
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
-	})
-
+	for _, test := range cases {
+		t.Run(test.Description, func(t *testing.T) {
+			got := ConvertToRoman(test.Arabic)
+			if got != test.Want {
+				t.Errorf("got %q, want %q", got, test.Want)
+			}
+		})
+	}
 }
