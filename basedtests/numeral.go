f0918bc58a967d704352e99a1fdad029a0d4a319
Chonlatee Jumratsee <chonlatee.j@lmwn.com>
2021-05-09 21:33:24 +0700

    Chapter: Intro to property based tests - Refactor check is 4 in loop

--- basedtests/numeral.go
+++ basedtests/numeral.go
@@ -4,13 +4,13 @@ import "strings"
 
 func ConvertToRoman(arabic int) string {
 
-	if arabic == 4 {
-		return "IV"
-	}
-
 	var result strings.Builder
 
-	for i := 0; i < arabic; i++ {
+	for i := arabic; i > 0; i-- {
+		if arabic == 4 {
+			result.WriteString("IV")
+			break
+		}
 		result.WriteString("I")
 	}
 
