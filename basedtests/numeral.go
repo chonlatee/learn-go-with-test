da4a1ff480fdd9c954ab6c00cd0f17ad3203245c
Chonlatee Jumratsee <chonlatee.j@lmwn.com>
2021-05-09 21:38:07 +0700

    Chapter: Intro to property based tests - Write enough code to make it pass check 5 in loop

--- basedtests/numeral.go
+++ basedtests/numeral.go
@@ -7,6 +7,11 @@ func ConvertToRoman(arabic int) string {
 	var result strings.Builder
 
 	for i := arabic; i > 0; i-- {
+		if arabic == 5 {
+			result.WriteString("V")
+			break
+		}
+
 		if arabic == 4 {
 			result.WriteString("IV")
 			break
