d86213dc5b6730d66cbef3e816e41c5e6bbf9397
Chonlatee Jumratsee <chonlatee.j@lmwn.com>
2021-05-09 21:04:44 +0700

    Chapter: Intro to property based tests - Write the test first write sub test for test 1 and 2

--- basedtests/numeral_test.go
+++ basedtests/numeral_test.go
@@ -3,10 +3,23 @@ package numeral
 import "testing"
 
 func TestRomanNumerals(t *testing.T) {
-	got := ConvertToRoman(1)
-	want := "I"
 
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
-	}
+	t.Run("1 gets converted to I", func(t *testing.T) {
+		got := ConvertToRoman(1)
+		want := "I"
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+
+	t.Run("2 gets converted to II", func(t *testing.T) {
+		got := ConvertToRoman(2)
+		want := "II"
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+
 }
