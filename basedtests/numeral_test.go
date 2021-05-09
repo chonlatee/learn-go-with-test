9d024ca4207fc9a4eb8ccca9091e03a330cc1345
Chonlatee Jumratsee <chonlatee.j@lmwn.com>
2021-05-09 21:34:26 +0700

    Chapter: Intro to property based tests - Write the test first add test case test 5

--- basedtests/numeral_test.go
+++ basedtests/numeral_test.go
@@ -12,6 +12,7 @@ func TestRomanNumerals(t *testing.T) {
 		{"2 gets converted to II", 2, "II"},
 		{"3 gets converted to III", 3, "III"},
 		{"4 gets converted to IV", 4, "IV"},
+		{"5 gets converted to V", 5, "V"},
 	}
 
 	for _, test := range cases {
